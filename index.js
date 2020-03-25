8a1c8e2024e766b07c5e79248779138e930bd8cb
Xavier <43008089+xm94@users.noreply.github.com>
2020-03-24 23:55:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -30,6 +30,12 @@ var app = express();
 // Serve static assets from the /public folder
 app.use('/public', express.static(path.join(__dirname, '/public')));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 // Serve the Parse API on the /parse URL prefix
 var mountPath = process.env.PARSE_MOUNT || '/parse';
 app.use(mountPath, api);
