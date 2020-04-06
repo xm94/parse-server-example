ecbd04375c8f7ce256aa3a89cb825c687972ec95
Xavier <43008089+xm94@users.noreply.github.com>
2020-04-05 23:58:42 -0600

    Update index.js

--- index.js
+++ index.js
@@ -20,6 +20,30 @@ var api = new ParseServer({
   liveQuery: {
     classNames: ["ConvertedProjects"] // List of classes to support for query subscriptions
   },
+  // Enable email verification
+  verifyUserEmails: true,
+
+  // if `verifyUserEmails` is `true` and
+  //     if `emailVerifyTokenValidityDuration` is `undefined` then
+  //        email verify token never expires
+  //     else
+  //        email verify token expires after `emailVerifyTokenValidityDuration`
+  //
+  // `emailVerifyTokenValidityDuration` defaults to `undefined`
+  //
+  // email verify token below expires in 2 hours (= 2 * 60 * 60 == 7200 seconds)
+  emailVerifyTokenValidityDuration: 2 * 60 * 60, // in seconds (2 hours = 7200 seconds)
+
+  // set preventLoginWithUnverifiedEmail to false to allow user to login without verifying their email
+  // set preventLoginWithUnverifiedEmail to true to prevent user from login if their email is not verified
+  preventLoginWithUnverifiedEmail: false, // defaults to false
+
+  // The public URL of your app.
+  // This will appear in the link that is used to verify email addresses and reset passwords.
+  // Set the mount path as it is in serverURL
+  publicServerURL: 'https://parabeac.com/parse',
+  // Your apps name. This will appear in the subject and body of the emails that are sent.
+  appName: 'Parabeac',
   emailAdapter: {
     module: 'parse-server-mandrill-adapter',
     options: {
