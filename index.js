954ff2739fba9fda499c49fc0116739f3df4ee56
Xavier <43008089+xm94@users.noreply.github.com>
2020-04-06 00:05:19 -0600

    Update index.js

--- index.js
+++ index.js
@@ -90,7 +90,7 @@ var api = new ParseServer({
       //verificationTemplateName: 'email-verification-template-name',
 
     },
-  }
+  },
 });
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
