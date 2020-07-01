d60c32b7a593eac905f0e0c825ec01f99c4194c7
Xavier <43008089+xm94@users.noreply.github.com>
2020-06-30 22:43:02 -0600

    updated live query

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ var api = new ParseServer({
   masterKey: process.env.MASTER_KEY || '123123123', //Add your master key here. Keep it secret!
   serverURL: process.env.SERVER_URL || 'http://localhost:1337/parse',  // Don't forget to change to https if needed
   liveQuery: {
-    classNames: ["ConvertedProjects"] // List of classes to support for query subscriptions
+    classNames: ["ConvertedProjects","BuildTest","Build","Project"] // List of classes to support for query subscriptions
   },
   // Enable email verification
   verifyUserEmails: true,
